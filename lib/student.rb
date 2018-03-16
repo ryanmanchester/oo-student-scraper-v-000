ed07b2bd4fad73fe0d6c5ae6a3f61cf467d78c24
Ryan Manchester <ryanm17@gmail.com>
2018-03-16 00:26:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,23 +8,19 @@ class Student
   def initialize(student_hash)
     student_hash.each {|k, v| self.send(("#{k}="), v)}
     @@all << self
-
   end
 
   def self.create_from_collection(students_array)
     #binding.pry
     students_array.collect {|student| Student.new(student)}
-
   end
 
   def add_student_attributes(attributes_hash)
     #binding.pry
     attributes_hash.each {|k, v| self.send(("#{k}="), v)}
-
-
   end
 
   def self.all
-
+    @@all
   end
 end
